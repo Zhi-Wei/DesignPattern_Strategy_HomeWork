eae667a8fd3d766cc6c7b4b492816bd43756ae44
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-26 23:22:52 +0800

    新建 IDeliveryService 定義宅配服務方法的介面。

---
+++ NineYi.Mall/BL/Interfaces/IDeliveryService.cs
@@ -0,0 +1,17 @@
+﻿using NineYi.Mall.BE.Entities;
+
+namespace NineYi.Mall.BL.Interfaces
+{
+    /// <summary>
+    /// 定義宅配服務的方法。
+    /// </summary>
+    public interface IDeliveryService
+    {
+        /// <summary>
+        /// 計算指定宅配項目的運費。
+        /// </summary>
+        /// <param name="deliveryItem">宅配項目。</param>
+        /// <returns>運費。</returns>
+        double Calculate(DeliveryEntity deliveryItem);
+    }
+}
