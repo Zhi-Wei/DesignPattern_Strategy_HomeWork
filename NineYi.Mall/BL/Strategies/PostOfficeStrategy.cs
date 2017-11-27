46733be6ecdaea0e3ce6171f9caafea0d6c9c05c
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-27 08:03:01 +0800

    新建 PostOfficeStrategy 郵局運費計算策略的類別。

---
+++ NineYi.Mall/BL/Strategies/PostOfficeStrategy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NineYi.Mall.BE.Entities;
+using NineYi.Mall.BE.Enums;
+using NineYi.Mall.BL.Interfaces;
+
+namespace NineYi.Mall.BL.Strategies
+{
+    /// <summary>
+    /// 郵局運費計算策略。
+    /// </summary>
+    /// <seealso cref="NineYi.Mall.BL.Interfaces.IFeeCalculationStrategy" />
+    public class PostOfficeStrategy : IFeeCalculationStrategy
+    {
+        /// <summary>
+        /// 計算指定宅配項目的運費。
+        /// </summary>
+        /// <param name="deliveryItem">宅配項目。</param>
+        /// <returns>
+        /// 運費。
+        /// </returns>
+        /// <exception cref="ArgumentException">請檢查 deliveryItem 參數。</exception>
+        public double Calculate(DeliveryEntity deliveryItem)
+        {
+            if (deliveryItem?.DeliveryType != DeliveryTypeEnum.PostOffice)
+            {
+                throw new ArgumentException($"請檢查 {nameof(deliveryItem)} 參數。");
+            }
+
+            double feeByWeight = deliveryItem.ProductWeight * 10 + 80;
+            double feeByVolumetric =
+                deliveryItem.ProductLength * deliveryItem.ProductWidth * deliveryItem.ProductHeight * 0.00001 * 110;
+
+            double fee = Math.Max(feeByWeight, feeByVolumetric);
+
+            return fee;
+        }
+    }
+}
