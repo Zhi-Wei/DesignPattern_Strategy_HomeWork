2f1c94368ae8ce32810cc6d02c6a2998caaf35d2
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-28 00:23:43 +0800

    1. 在 DeliveryService 類別增加繼承 IDeliveryService 介面。 2. 調整 DeliveryService 類別的 XML 註解。 3. 重構 DeliveryService.Calculate 方法。

--- NineYi.Mall/BL/DeliveryService.cs
+++ NineYi.Mall/BL/DeliveryService.cs
@@ -1,72 +1,34 @@
 ﻿using System;
 using NineYi.Mall.BE.Entities;
-using NineYi.Mall.BE.Enums;
+using NineYi.Mall.BL.Factories;
+using NineYi.Mall.BL.Interfaces;
 
 namespace NineYi.Mall.BL
 {
     /// <summary>
-    /// 宅配Service
+    /// 宅配服務。
     /// </summary>
-    public class DeliveryService
+    public class DeliveryService : IDeliveryService
     {
         /// <summary>
-        /// 計算運費
+        /// 計算運費。
         /// </summary>
-        /// <param name="deliveryItem">宅配資料</param>
-        /// <returns>運費</returns>
+        /// <param name="deliveryItem">宅配資料。</param>
+        /// <returns>
+        /// 運費。
+        /// </returns>
+        /// <exception cref="ArgumentNullException">deliveryItem - 請檢查 deliveryItem 參數。</exception>
         public double Calculate(DeliveryEntity deliveryItem)
         {
             if (deliveryItem == null)
             {
-                throw new ArgumentException("請檢查 deliveryItem 參數");
+                throw new ArgumentNullException(nameof(deliveryItem), $"請檢查 {nameof(deliveryItem)} 參數。");
             }
 
-            var fee = default(double);
-            if (deliveryItem.DeliveryType == DeliveryTypeEnum.TCat)
-            {
-                var weight = deliveryItem.ProductWeight;
-                if (weight > 20)
-                {
-                    fee = 400d;
-                }
-                else
-                {
-                    fee = 100 + weight * 10;
-                }
+            var strategy = StrategyFactory.CreateStrategy(deliveryItem.DeliveryType);
+            double fee = strategy.Calculate(deliveryItem);
 
-                return fee;
-            }
-            else if (deliveryItem.DeliveryType == DeliveryTypeEnum.KTJ)
-            {
-                var length = deliveryItem.ProductLength;
-                var width = deliveryItem.ProductWidth;
-                var height = deliveryItem.ProductHeight;
-
-                var size = length * width * height;
-
-                if (length > 50 || width > 50 || height > 50)
-                {
-                    fee = size * 0.00001 * 110 + 50;
-                }
-                else
-                {
-                    fee = size * 0.00001 * 120;
-                }
-
-                return fee;
-            }
-            else if (deliveryItem.DeliveryType == DeliveryTypeEnum.PostOffice)
-            {
-                double feeByWeight = deliveryItem.ProductWeight * 10 + 80;
-                double feeByVolumetric =
-                    deliveryItem.ProductLength * deliveryItem.ProductWidth * deliveryItem.ProductHeight * 0.00001 * 110;
-
-                fee = Math.Max(feeByWeight, feeByVolumetric);
-
-                return fee;
-            }
-
-            throw new ArgumentException("請檢查 deliveryItem.DeliveryType 參數");
+            return fee;
         }
     }
 }
