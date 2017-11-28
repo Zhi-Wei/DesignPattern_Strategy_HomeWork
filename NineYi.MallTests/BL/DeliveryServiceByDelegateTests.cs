5a70b9431c578411ae9337c444b06b7225d94a46
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-28 15:49:09 +0800

    新增 DeliveryServiceByDelegate.CalculatePostOfficeShippingFee 私有方法的測試案例。

--- NineYi.MallTests/BL/DeliveryServiceByDelegateTests.cs
+++ NineYi.MallTests/BL/DeliveryServiceByDelegateTests.cs
@@ -392,5 +392,98 @@ namespace NineYi.Mall.BL.Tests
         }
 
         #endregion -- CalculateKtjShippingFee --
+
+        #region -- CalculatePostOfficeShippingFee --
+
+        [Fact]
+        [Trait(nameof(DeliveryServiceByDelegate), "")]
+        [Trait(nameof(DeliveryServiceByDelegate), "CalculatePostOfficeShippingFee")]
+        public void CalculatePostOfficeShippingFee_當傳入參數deliveryItem為Null時_應拋出ArgumentException的例外狀況()
+        {
+            // Arrange
+            DeliveryEntity deliveryItem = null;
+            var message = $"請檢查 deliveryItem 參數。{Environment.NewLine}參數名稱: deliveryItem";
+            var sut = new PrivateType(typeof(DeliveryServiceByDelegate));
+
+            // Act
+            Action action = () => sut.InvokeStatic("CalculatePostOfficeShippingFee", deliveryItem);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>().And.Message.Should().Be(message);
+        }
+
+        [Fact]
+        [Trait(nameof(DeliveryServiceByDelegate), "")]
+        [Trait(nameof(DeliveryServiceByDelegate), "CalculatePostOfficeShippingFee")]
+        public void CalculatePostOfficeShippingFee_當傳入參數DeliveryType不為PostOffice時_應拋出ArgumentException的例外狀況()
+        {
+            // Arrange
+            var deliveryItem = new DeliveryEntity()
+            {
+                ProductLength = 30,
+                ProductWidth = 40,
+                ProductHeight = 50,
+                ProductWeight = 25,
+                DeliveryType = (DeliveryTypeEnum)99
+            };
+            var message = $"請檢查 deliveryItem 參數。{Environment.NewLine}參數名稱: deliveryItem";
+            var sut = new PrivateType(typeof(DeliveryServiceByDelegate));
+
+            // Act
+            Action action = () => sut.InvokeStatic("CalculatePostOfficeShippingFee", deliveryItem);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>().And.Message.Should().Be(message);
+        }
+
+        [Fact]
+        [Trait(nameof(DeliveryServiceByDelegate), "")]
+        [Trait(nameof(DeliveryServiceByDelegate), "CalculatePostOfficeShippingFee")]
+        public void CalculatePostOfficeShippingFee_當傳入參數DeliveryType為PostOffice且依重量計算的運費大於依材積計算的運費時_應回傳依重量計算的運費()
+        {
+            // Arrange
+            var deliveryItem = new DeliveryEntity()
+            {
+                ProductLength = 30,
+                ProductWidth = 40,
+                ProductHeight = 50,
+                ProductWeight = 25,
+                DeliveryType = DeliveryTypeEnum.PostOffice
+            };
+            double expected = 330;
+            var sut = new PrivateType(typeof(DeliveryServiceByDelegate));
+
+            // Act
+            double actual = (double)sut.InvokeStatic("CalculatePostOfficeShippingFee", deliveryItem);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        [Trait(nameof(DeliveryServiceByDelegate), "")]
+        [Trait(nameof(DeliveryServiceByDelegate), "CalculatePostOfficeShippingFee")]
+        public void CalculatePostOfficeShippingFee_當傳入參數DeliveryType為PostOffice且依重量計算的運費小於依材積計算的運費時_應回傳依材積計算的運費()
+        {
+            // Arrange
+            var deliveryItem = new DeliveryEntity()
+            {
+                ProductLength = 60,
+                ProductWidth = 60,
+                ProductHeight = 80,
+                ProductWeight = 15,
+                DeliveryType = DeliveryTypeEnum.PostOffice
+            };
+            double expected = 316.8;
+            var sut = new PrivateType(typeof(DeliveryServiceByDelegate));
+
+            // Act
+            double actual = (double)sut.InvokeStatic("CalculatePostOfficeShippingFee", deliveryItem);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        #endregion -- CalculatePostOfficeShippingFee --
     }
 }
