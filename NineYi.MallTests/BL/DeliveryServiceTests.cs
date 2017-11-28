be11df91a6493c812bbc660e2bb5b7a71e3da31f
Zhi-Wei <ZhiWei.Lu@outlook.com>
2017-11-28 16:22:36 +0800

    增加 DeliveryService.Calculate 方法的測試案例。

--- NineYi.MallTests/BL/DeliveryServiceTests.cs
+++ NineYi.MallTests/BL/DeliveryServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using NineYi.Mall.BE.Entities;
 using NineYi.Mall.BE.Enums;
@@ -8,6 +9,13 @@ namespace NineYi.Mall.BL.Tests
 {
     public class DeliveryServiceTests
     {
+        #region -- 前置準備 --
+
+        private DeliveryService GetSystemUnderTestInstance()
+        {
+            return new DeliveryService();
+        }
+
         /// <summary>
         /// 宅配資料For黑貓
         /// </summary>
@@ -131,21 +139,69 @@ namespace NineYi.Mall.BL.Tests
             }
         }
 
+        #endregion -- 前置準備 --
+
+        #region -- Calculate --
+
         [Theory]
         [MemberData(nameof(DeliveryItemForTCat))]
         [MemberData(nameof(DeliveryItemForKTJ))]
         [MemberData(nameof(DeliveryItemForPostOffice))]
+        [Trait(nameof(DeliveryService), "")]
+        [Trait(nameof(DeliveryService), "Calculate")]
         public void Test_Calculate(DeliveryEntity deliveryItem, double expected)
         {
-            //// Arrange
-            var actual = 0d;
-            var target = new DeliveryService();
+            // Arrange
+            var sut = this.GetSystemUnderTestInstance();
 
-            //// Act
-            actual = target.Calculate(deliveryItem);
+            // Act
+            double actual = sut.Calculate(deliveryItem);
 
-            //// Arrange
-            expected.Should().Be(actual);
+            // Arrange
+            actual.Should().Be(expected);
         }
+
+        [Fact]
+        [Trait(nameof(DeliveryService), "")]
+        [Trait(nameof(DeliveryService), "Calculate")]
+        public void Calculate_當傳入參數deliveryItem為Null時_應拋出ArgumentNullException的例外狀況()
+        {
+            // Arrange
+            DeliveryEntity deliveryItem = null;
+            var message = $"請檢查 deliveryItem 參數。{Environment.NewLine}參數名稱: deliveryItem";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            Action action = () => sut.Calculate(deliveryItem);
+
+            // Assert
+            action.ShouldThrow<ArgumentNullException>().And.Message.Should().Be(message);
+        }
+
+        [Fact]
+        [Trait(nameof(DeliveryService), "")]
+        [Trait(nameof(DeliveryService), "Calculate")]
+        public void Calculate_當傳入參數DeliveryType為未定義宅配策略的值時_應拋出ArgumentException的例外狀況()
+        {
+            // Arrange
+            var deliveryItem = new DeliveryEntity()
+            {
+                ProductLength = 30,
+                ProductWidth = 40,
+                ProductHeight = 50,
+                ProductWeight = 25,
+                DeliveryType = (DeliveryTypeEnum)99
+            };
+            var message = $"請檢查 deliveryType 參數。{Environment.NewLine}參數名稱: deliveryType";
+            var sut = this.GetSystemUnderTestInstance();
+
+            // Act
+            Action action = () => sut.Calculate(deliveryItem);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>().And.Message.Should().Be(message);
+        }
+
+        #endregion -- Calculate --
     }
 }
